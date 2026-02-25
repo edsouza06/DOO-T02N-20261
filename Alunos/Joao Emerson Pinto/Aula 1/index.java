3ecb58fec41aed8711ffe4d2a157b8aecfa875de
JoaoEmersonPinto <jepinto@minha.fag.edu.br>
2026-02-25 20:37:33 -0300

    First Commit

---
+++ Alunos/Joao Emerson Pinto/Aula 1/index.java
@@ -0,0 +1,11 @@
+public class index {
+
+    public static void main(String[] args) {    
+    
+        System.out.println("Hello Word!");
+        System.out.println("Hi Joao Emerson Pinto!!");
+    
+    }
+
+}
+
