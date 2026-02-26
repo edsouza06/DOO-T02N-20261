cd4b9bd44182fff5d017ced2f07ceb2282cab267
Kaua <kauabenini89@gmail.com>
2026-02-25 21:07:42 -0300

    Kaua

---
+++ Alunos/Kauã Cardoso Benini/Aula01/App.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Diga seu nome");
+        String nome = scan.nextLine();
+        System.out.println(nome + " Hello Word!");
+
+    }
+}
